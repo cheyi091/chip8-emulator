d4ff3c71c876c269917c7eaa2dd322d924ed9d0d
cheyyi <joeylin.cheyi@gmail.com>
2022-05-20 12:32:37 +0800

    Draw sprites to the screen

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main(int argc, char** argv) {
     struct chip8 chip8;
     chip8_init(&chip8);
     
-    chip8_screen_set(&chip8.screen, 0, 0);
+    chip8_screen_draw_sprite(&chip8.screen, 32, 30, &chip8.memory.memory[0x00], 5);
 
     // Create a window
     SDL_Init(SDL_INIT_EVERYTHING);
